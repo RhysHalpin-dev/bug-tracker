0efedc97a6a12cf86acfd221d0aad6cc27de7503
RhysHalpin-dev <rhyshalpin@hotmail.co.uk>
2022-07-20 21:46:59 +0100

    removed AuthorizationJwt as code redundant

--- bug-tracker-api/cmd/controller/authHandler.go
+++ bug-tracker-api/cmd/controller/authHandler.go
@@ -26,7 +26,7 @@ func CheckPasswordHash(password, hash string) bool {
 }
 
 /*
-	Generate a JWT Token uisng user data
+	Generate a JWT Token using user data
 */
 func generateToken(mongoUser bson.M) (tokenString string) {
 
@@ -41,32 +41,12 @@ func generateToken(mongoUser bson.M) (tokenString string) {
 	// Sign and get the complete encoded token as a string using the secret
 	tokenString, err := token.SignedString(hmacSecretKey)
 	fmt.Println(tokenString, err)
-	AuthorizationJwt(tokenString)
 	return tokenString
 
 }
 
-func AuthorizationJwt(jwtToken string) {
-	hmacSecretKey := []byte(os.Getenv("SECRETKEY")) //jwt secret signing key stored in env
-
-	token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
-
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return hmacSecretKey, nil
-	})
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		fmt.Println(claims["userName"], claims["admin"])
-	} else {
-		fmt.Println(err)
-	}
-}
-
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		// take jwt token from request
 		reqToken := r.Header.Get("Authorization")
 		jwtToken := strings.Split(reqToken, "Bearer")
